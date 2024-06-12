55acef68204866856a1ad8b9a579e139b2edfce0
ujjwalbharadwaj <ujjwalbhardwaj5809@gmail.com>
2024-06-12 15:41:23 +0530

    updated featured product style

--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -24,9 +24,9 @@ const FeaturedProduct = () => {
         >
           {allProducts && allProducts.length !== 0 && (
             <>
-              {allProducts.map((product, index) => (
-                <div className="snap-center shrink-0" key={index}>
-                  <ProductCard data={product} />
+              {allProducts.map((i, index) => (
+                <div className="snap-center shrink-0 w-1/2 md:w-1/4 lg:w-1/6 xl:w-1/6" key={index}>
+                  <ProductCard data={i} />
                 </div>
               ))}
               </>
